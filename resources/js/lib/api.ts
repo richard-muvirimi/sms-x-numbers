4b45b56f53c7904f929149291bc1b1bafdb3b52d
richard-muvirimi <19529979+richard-muvirimi@users.noreply.github.com>
2025-05-27 07:43:28 +0000

    fix code style

--- resources/js/lib/api.ts
+++ resources/js/lib/api.ts
@@ -1,33 +1,30 @@
-import '../bootstrap';
 import type { AxiosRequestConfig } from 'axios';
+import '../bootstrap';
 
 declare global {
-  interface Window {
-    axios: typeof import('axios').default;
-  }
+    interface Window {
+        axios: typeof import('axios').default;
+    }
 }
 
-export async function fetchApi<T>(
-  url: string,
-  options: Omit<AxiosRequestConfig, 'url'> = {}
-): Promise<T> {
-  try {
-    // Set default headers for this request
-    const headers = {
-      'Accept': 'application/json',
-      ...options.headers,
-    };
+export async function fetchApi<T>(url: string, options: Omit<AxiosRequestConfig, 'url'> = {}): Promise<T> {
+    try {
+        // Set default headers for this request
+        const headers = {
+            Accept: 'application/json',
+            ...options.headers,
+        };
 
-    const response = await window.axios.request<T>({
-      url,
-      ...options,
-      headers,
-    });
-    return response.data;
-  } catch (error) {
-    if (window.axios.isAxiosError(error) && error.response?.data?.message) {
-      throw new Error(error.response.data.message);
+        const response = await window.axios.request<T>({
+            url,
+            ...options,
+            headers,
+        });
+        return response.data;
+    } catch (error) {
+        if (window.axios.isAxiosError(error) && error.response?.data?.message) {
+            throw new Error(error.response.data.message);
+        }
+        throw error;
     }
-    throw error;
-  }
 }
